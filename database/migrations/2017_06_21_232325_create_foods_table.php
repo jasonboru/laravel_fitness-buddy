036ff377f3a9745586bfa491e2b971b38c96dd3f
Jason O'Brien <jasonobrien@jasons-mbp.attlocal.net>
2017-06-24 16:02:32 -0400

    added migrations for meals and foods

---
+++ database/migrations/2017_06_21_232325_create_foods_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('protien')->unsigned();
+            $table->integer('carbohydrates')->unsigned();
+            $table->integer('fat')->unsigned();
+            $table->integer('meal_id')->unsigned()->index();
+            //$table->foreign('meal_id')->references('id')->on('meals');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('foods');
+    }
+}
